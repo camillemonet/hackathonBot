e5c691a1110ad1c84e72529aa4116cf381a4caf6
k3vross <k3vross@gmail.com>
2020-10-20 09:37:03 -0700

    add more dialogue

--- features/kevin.js
+++ features/kevin.js
@@ -14,8 +14,12 @@ module.exports = function (controller) {
                         payload: 'coding projects',
                     },
                     {
-                        title: 'proficiencies',
+                        title: 'Proficiencies',
                         payload: 'proficiencies'
+                    },
+                    {
+                        title: 'Speak to Someone Else',
+                        payload: 'speak to someone else'
                     }
                 ]
             });
@@ -26,11 +30,56 @@ module.exports = function (controller) {
   });
 
   controller.hears(async (message) => message.text && message.text.toLowerCase() === 'coding projects' && controller._config.currentSpeaker === "kevin", ['message', 'direct_message'], async (bot, message) => {
-    await bot.reply(message, `My three projects are a fullstack Splitwise clone using Ruby on Rails, JavaScript, React, and Redux...`)
+    await bot.reply(message, {
+      text: 'I have been involved in three major projects. SplitCash: A fullstack expense sharing app using Rubyr, Rails, React, Redux, & PostgresSQL. Volunteerist: A group project using the MERN stack where I was the lead frontend engineer and developed the React components. MazeR: A 2-d maze platformer created using JavaScript, canvas, HTML5, and CSS3. Which one would you like to hear about?',
+      quick_replies: [
+        {
+          title: 'SplitCash',
+          payload: 'SplitCash'
+        },
+        {
+          title: 'Vlunteerist',
+          payload: 'Volunteerist'
+        },
+        {
+          title: 'MazeR',
+          payload: 'MazeR'
+        },
+        {
+          title: 'Back to my Overview',
+          payload: 'kevin'
+        }
+      ]
+    })
   });
 
   controller.hears(async (message) => message.text && message.text.toLowerCase() === 'proficiencies' && controller._config.currentSpeaker === "kevin", ['message', 'direct_message'], async (bot, message) => {
     await bot.reply(message, `I am proficient in Ruby, Rails, JavaScript, Node.js, React, Redux, PostgresSQL, MongoDB, CSS3, and HTML5`)
+  });
+
+
+  controller.hears(async (message) => message.text && message.text.toLowerCase() === 'speak to someone else' && controller._config.currentSpeaker === "kevin", ['message', 'direct_message'], async (bot, message) => {
+    await bot.reply(message, {
+      text: 'Who would you like to speak with?',
+      quick_replies: [
+        {
+          title: 'Kevin',
+          payload: 'kevin'
+        },
+        {
+          title: 'Camille',
+          payload: 'camille'
+        },
+        {
+          title: 'Solomon',
+          payload: 'solomon'
+        },
+        {
+          title: 'Elijah',
+          payload: 'elijah'
+        },
+      ]
+    })
   });
 
 }
